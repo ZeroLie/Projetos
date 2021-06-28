aa77063aa1aa303a876783df8037ee654b7aec40
ZeroLie <fabianaakamine@hotmail.com>
2021-06-28 19:00:52 -0300

    Adicionado o código da aula 11 - HATEOAS

---
+++ 09 - REST_ADID Scaffold Hateoas/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,7 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Avenida Doce Filho, 3501', 'Fabiana Lie', 'Female', 'Akamine'),
+	(2, 'Rua dos Sei lá, 000', 'Alguém', 'Male', 'Silva'),
+	(3, 'Avenida Bizarro Pai, 2000', 'Renato', 'Male', 'Maia'),
+	(4, 'Rua dos doidos, 981', 'Zero', 'Male', 'Shirotsuki'),
+	(5, 'Rua das Margaridas, 720', 'Kira', 'Female', 'Santana'),
+	(6, 'Rua dos Titãs, 351', 'Mikasa', 'Female', 'Ackerman');
